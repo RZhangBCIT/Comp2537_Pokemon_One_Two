07672c1012afd72d27afa8c9dc065e4e7d6fe1be
Ray Zhang <rzhang104@my.bcit.ca>
2022-05-10 22:19:15 -0700

    added views folder and ejs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs');
 
 
 app.listen(2022, (err) => {
@@ -13,3 +14,8 @@ app.get('/', function (req, res) {
     res.send('GET request to homepage')
 })
 
+app.get('/profile/:id', function (req, res) {
+    res.render("profile.ejs", {
+        "id": req.params.id
+    })
+})
