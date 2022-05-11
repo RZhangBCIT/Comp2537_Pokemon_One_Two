bce3e596a271d916e4a42d624f2ec1900f243c12
Ray Zhang <rzhang104@my.bcit.ca>
2022-05-10 21:54:24 -0700

    added home page call to server.js

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const app = express()
+
+
+app.listen(2022, (err) => {
+    if (err) console.log(err);
+})
+
+/**
+ * req = request object, res = response object
+ */
+app.get('/', function (req, res) {
+    res.send('GET request to homepage')
+})
+
