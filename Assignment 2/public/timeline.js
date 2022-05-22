e635c993dc4dd5a96744951de85f50a6e0ac3262
Ray Zhang <rzhang104@my.bcit.ca>
2022-05-21 23:24:47 -0700

    added increase hits and delete portions to timeline

--- Assignment 2/public/timeline.js
+++ Assignment 2/public/timeline.js
@@ -3,20 +3,53 @@ function loadEventsToMainDiv() {
         url: "http://localhost:1444/timeline/getAllEvents",
         type: "get",
         success: (r) => {
-            console.log(r)
+            // console.log(r)
             for (i = 0; i < r.length; i++) {
                 $('main').append(`
-                    <p> Event Text ${r[i].text} </p>
-                    <p> Event Hits ${r[i].hits} </p>
-                    <p> Event Time ${r[i].time} </p>
+                    <p> ${r[i].text} </p>
+                    <p> ${r[i].time} </p>
+                    <p> Hits: ${r[i].hits} </p>
+
+                    <button class="likeButtons" id="${r[i]["_id"]}"> Like! </button>
+                    <button class="deleteButtons" id="${r[i]["_id"]}"> Delete </button>
                     `);
             }
         }
     })
 }
 
+function increaseHits() {
+    x = this.id
+    $.ajax({
+        url: `http://localhost:1444/timeline/increaseHits/${x}`,
+        type: "get",
+        success: function (x) {
+            console.log(x);
+        }
+    })
+    $('main').html('');
+    loadEventsToMainDiv()
+}
+
+function deleteID() {
+    x = this.id
+    $.ajax({
+        url: `http://localhost:1444/timeline/delete/${x}`,
+        type: "get",
+        success: x => {
+            console.log(x)
+        }
+    })
+    $('main').html('');
+    loadEventsToMainDiv()
+}
+
 function setup() {
     loadEventsToMainDiv();
+
+    $("body").on("click", ".likeButtons", increaseHits)
+
+    $("body").on("click", ".deleteButtons", deleteID)
 }
 
 $(document).ready(setup)
